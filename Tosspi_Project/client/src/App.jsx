cb45950357de28d078f0fd5be1c3597154316538
arnold jones <tosspinc@gmail.com>
2024-02-17 21:15:22 -0700

    updated code for level 3 sp 180.5 navbar and styling on 02.18.2024 at 0142 hours.

--- Tosspi_Project/client/src/App.jsx
+++ Tosspi_Project/client/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './App.css'
-import Navbar from './components/Header.jsx'
+import Header from './components/Header.jsx'
+import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
+
 
 function App() {
   return (
     <>
-      <div>
+      <div className='container'>
+        <Header />
         <h1>Tosspi - The One Stop Shopping Place Inc. My project page.</h1>
+        <MainContent />
         <Footer />
       </div>
     </>
--- Tosspi_Project/client/src/components/Header.jsx
+++ Tosspi_Project/client/src/components/Header.jsx
@@ -2,8 +2,10 @@ import React from "react"
 
 export default function Header() {
     return (
-        <header className="header">
-            <img src="./imgs/Tosspi_Logo.png" className="nav-logo" />
+        <header className="navbar-belt">
+            <nav className="navbar-main">
+                <img src="src/imgs/Transp_Tosspi_Logo.png" className="nav-logo" />
+            </nav>    
         </header>
     )
 }
