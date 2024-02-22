ae9016224c702ea2ef9fc35e1148b273e1c81874
arnold jones <tosspinc@gmail.com>
2024-02-22 14:48:19 -0700

    updatind code with help from TA angelique Mckay

--- Tosspi_Project/client/src/components/Header.jsx
+++ Tosspi_Project/client/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react"
+// import logo from "../imgs/Tosspi_Logo.png"
 
 export default function Header() {
     return (
         <header className="navbar-belt">
             <nav className="navbar-main">
                 <div className="nav-left">
-                <img src="./Tosspi_Logo.png" className="tosspi-bag-logo" />
-                <img src="./Trans_Tosspi_logo.png" className="nav-logo" />
+                <img src="./imgs/Tosspi_Logo.png" className="tosspi-bag-logo" />
+                <img src="./imgs/Transp_Tosspi_logo.png" className="nav-logo" />
                 </div>
             </nav>    
         </header>
