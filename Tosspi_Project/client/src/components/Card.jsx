e38a3c56e8ef1161f98c25410f7c6182e9a0ae73
arnold jones <tosspinc@gmail.com>
2024-02-28 20:05:48 -0700

    updated code and notation in the card.jsx and maincontent.jsx files to add according to assignment in level3 passing props. completed on 02.28.2024 at 2006hrs.

--- Tosspi_Project/client/src/components/Card.jsx
+++ Tosspi_Project/client/src/components/Card.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-export default function Card() {
-    return (
+export default function Card(props) {
+    return (        /*using a process called interpolation adding in the js code using the {}*/
         <div className="card">
-            <img src="./imgs/product-categories/tools.jpg" className="card-tool-image" />
+            <img src={'./imgs/product-categories/${props.img}'} className="card-tool-image" />
             <div className="card-reviews">
                 <img src="./imgs/product-reviews/five-star-review.png" className="five-star-review" />
-                <span className="gray"> 5.0  </span>
-                <span className="gray">(6) </span>
+                <span className="gray"> {props.rating} </span>
+                <span className="gray">({props.reviewCount}) </span>
             </div>
-            <p><span className="item-description">Every day tools.</span></p>
-            <p><span className="item-pricing">starting at $5.00 Each.</span></p>
+            <p><span className="item-description">{props.title}</span></p>
+            <p><span className="item-pricing">starting at ${props.price}.00 Each.</span></p>
         </div>
     )
 }
--- Tosspi_Project/client/src/components/MainContent.jsx
+++ Tosspi_Project/client/src/components/MainContent.jsx
@@ -8,7 +8,13 @@ export default function MainContent() {
         <div className="maincontent--info">
             <h1 className="maincontent--title">Level 3 React project!</h1>
             <h2>Working on class project.</h2>
-            <Card />
+            <Card 
+                img='tools.jpg'
+                ratings="5.0"
+                reviewCount={6}
+                title="Everyday Tools"
+                price={5}
+            />
             <Contacts />
        </div>
     )
