37c171142249c73320601d4ad7b6056843820215
arnold jones <tosspinc@gmail.com>
2024-03-03 11:43:59 -0700

    updated level3 sp200 code to display correctly. updtated on 03.03.2024 at 1144hours.

--- Level3/Lessons/sp200_Card_Component_Lesson/client/src/App.jsx
+++ Level3/Lessons/sp200_Card_Component_Lesson/client/src/App.jsx
@@ -23,7 +23,9 @@ export default function App() {
   return (
     <div>
       <Navbar />
-      {cards}    
+      <section className='cards-list'>
+        {cards}    
+      </section>   
     </div>
   )
 }
