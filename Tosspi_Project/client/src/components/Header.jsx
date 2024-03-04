017d430d905478c401af1f5daabd50da79a738e5
arnold jones <tosspinc@gmail.com>
2024-03-03 18:23:22 -0700

    updated code for personal project in level3 assignment on 03.04.2024 at 2357 hours.

--- Tosspi_Project/client/src/components/Header.jsx
+++ Tosspi_Project/client/src/components/Header.jsx
@@ -7,15 +7,31 @@ export default function Header() {
         <header className="navbar-header">
             <nav className="navbar-main">
                 <div className="nav-left">
-                <img src="./imgs/Tosspi_Logo.png" className="tosspi-bag-logo" />
-                <img src="./imgs/Transp_Tosspi_logo.png" className="nav-logo" />
-                
-                </div>
-                <div className="nav-center">
-
+                    <img src="./imgs/Tosspi_Logo.png" className="tosspi-bag-logo" />
+                    <img src="./imgs/Transp_Tosspi_logo.png" className="nav-logo" />
+                    </div>
+                <div className="nav-search">
+                    <div className="box">
+                        <button type="submit" className="all"> All <i className="fa fa-sort-desc"></i></button>
+                        <input type="text" /><button type="submit" className="search"><i className="fa fa-search"></i></button>
+                    </div>
                 </div>
                 <div className="nav-right">
-                <img src="./imgs/Login.jpg" className="login-logo" />
+                    <div className="item">
+                        <img src="./imgs/flag-usa.jpg" className="flag-usa" />
+                    </div>
+                    <div className="item">
+                        <p>Hello!</p>
+                        <h5>Account & Lists
+                        <i className="fa fa-sort-desc"></i></h5>
+                    </div>
+                    <div className="item">
+                        <p>Returns</p>
+                        <h5>and Orders</h5>
+                    </div>
+                    <div className="item">
+                        <i className="fa fa-shopping-cart fa-2x"></i>Cart</div>
+                        <img src="./imgs/Login.jpg" className="login-logo" />
                 </div>
                 <div className="navbar-categories">
                     <ProductCategories />
