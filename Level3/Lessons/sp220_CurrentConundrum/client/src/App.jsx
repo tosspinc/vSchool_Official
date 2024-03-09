c3537f5974575becd900b5bf181171e284f9bf27
arnold jones <tosspinc@gmail.com>
2024-03-08 18:38:35 -0700

    completed coding for level 3 sp220 current conundrum lesson.  completed on 03.08.2024 at 1839 hours.

--- Level3/Lessons/sp220_CurrentConundrum/client/src/App.jsx
+++ Level3/Lessons/sp220_CurrentConundrum/client/src/App.jsx
@@ -1,13 +1,21 @@
+import React from 'react';
 import './App.css'
 
-function App() {
+export default function App() {
+  const [things, setThings] = React.useState(["Things 1", "Things 2"])
+  
+  function addAnItem() {
+  const newThingText = `Things ${things.length + 1}`
+  setThings(prevState => [...prevState, newThingText])
+  }
+
+  const thingsElements = things.map(thing => <p key={thing}>{thing}</p>)
   return (
     <>
       <div>
-      
+        <button onClick={addAnItem}>Add an Item.</button> 
+        {thingsElements}     
       </div>     
     </>
   )
 }
-
-export default App
