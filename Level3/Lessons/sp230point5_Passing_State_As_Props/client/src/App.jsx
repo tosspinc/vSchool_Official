36c11917ad9f298a2815b9f92c8b8f6ff2106785
arnold jones <tosspinc@gmail.com>
2024-03-15 22:51:05 -0600

    completed lvl3 sp230.5 passing state, get errors and need to figure out issue as not rendering in dom. completed 03.15.2024 at 2251hours.

---
+++ Level3/Lessons/sp230point5_Passing_State_As_Props/client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Count from './components/Count'
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = React.useState(0)
+
+  function add() {
+    setCount (prevCount => prevCount + 1)
+  }
+
+  function subtract() {
+    setCount (prevCount => prevCount - 1)
+  }
+
+  return (
+    <div className='counter'>
+      <button className='counter--minus' onClick={subtract}>-</button>
+      <Count number={count} />
+      <button className='counter--plus' onClick={add}>+</button>
+    </div>
+  )
+}
