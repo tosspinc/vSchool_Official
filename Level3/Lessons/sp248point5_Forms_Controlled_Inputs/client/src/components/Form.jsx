8b01eea4ed14d1155a8f6feaa2b773bebdd857ec
arnold jones <tosspinc@gmail.com>
2024-03-29 13:20:08 -0600

    updated coding by adding a controlled input for each data point (item) that I want to save. completed on 03.29.2024 at 1320 hours.

---
+++ Level3/Lessons/sp248point5_Forms_Controlled_Inputs/client/src/components/Form.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default function Form() {
+    const [formData, setFormData] = React.useState(
+        {firstName: '', lastName: '', email: ''}
+    )
+    
+    console.log(formData)          
+
+    function handleChange(event) {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
+        return (
+        <form>
+            <input 
+                type='text'
+                placeholder='First Name'
+                onChange={handleChange}
+                name='firstName'
+                value={formData.firstName}
+
+            />
+            <input 
+                type='text'
+                placeholder='Last Name'
+                onChange={handleChange}
+                name='lastName'
+                value={formData.Form}
+            />
+            <input
+                type='email'
+                placeholder='Email'
+                onChange={handleChange}
+                email='email'
+                value={formData.email}
+            />
+        </form>
+    )
+}
