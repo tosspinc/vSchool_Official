c44d79cc421a72729542fc02a39ffb8678c04f58
arnold jones <tosspinc@gmail.com>
2024-03-29 16:37:47 -0600

    completed coding and notation for level 3 assignment involving forms using react. completed on 04.01.2024 at 2033hours.

--- Level3/Lessons/sp249_251_Forms_In_React/client/src/components/Form.jsx
+++ Level3/Lessons/sp249_251_Forms_In_React/client/src/components/Form.jsx
@@ -7,7 +7,10 @@ export default function Form() {
             lastName: '', 
             email: '', 
             comments:'',
-            isFriendly: false
+            isFriendly: false,
+            employment: '',
+            favColor: '',
+
         }
     )
     
@@ -21,8 +24,13 @@ export default function Form() {
         })
     }
 
+        function handleSubmit(event) {
+            event.preventDefault()
+            console.log(formData)
+        }
+
         return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input 
                 type='text'
                 placeholder='First Name'
@@ -59,6 +67,66 @@ export default function Form() {
                     onChange={handleChange}
                 />Are you a friendly person?
             </label>
+            <br />
+            <br />
+
+            <fieldset>
+                <legend>Current employment status</legend>
+                <input
+                    type="radio"
+                    id="unemployed" 
+                    name="employment"
+                    value='unemployed'
+                    checked={formData.employment === 'unemployed'}
+                    onChange={handleChange}
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+
+                <input
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    value='part-time'
+                    checked={formData.employment === 'part-time'}
+                    onChange={handleChange}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+
+                <input
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    value='full-time'
+                    checked={formData.employment === 'full-time'}
+                    onChange={handleChange}
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />
+            </fieldset>
+
+            <label htmlFor="favColor">What is your favorite color?</label>
+            <br />
+            <select 
+                id="favColor"
+                value={formData.favColor}
+                onChange={handleChange}
+                name='favColor'
+                >
+                <option value=''>--Choose Color--</option>
+                <option value='red'>Red</option>
+                <option value='orange'>Orange</option>
+                <option value='yellow'>Yellow</option>
+                <option value='green'>Green</option>
+                <option value='blue'>Blue</option>
+                <option value='indigo'>Indigo</option>
+                <option value='violet'>Violet</option>
+
+            </select>
+            <br />
+            <br />
+            <button>Submit</button>
         </form>
     )
 }
